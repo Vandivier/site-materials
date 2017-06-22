9084cee0fefc020d9234572c955da5de0e769cd5
Vandivier <vandivier_john@yahoo.com>
2017-06-21 23:33:36 -0400

    starting to make it an ng app

--- afterecon-ssp.php
+++ afterecon-ssp.php
@@ -13,9 +13,9 @@ include_once(dirname(__FILE__) . '/data_demo.php');
 
 add_action('wp_footer','mod_foot_ssp');
 function mod_foot_ssp() {
-  /* ref: https://stackoverflow.com/a/20717674/3931488 */
 
-  $url = "https://raw.githubusercontent.com/Vandivier/site-materials/master/afterecon-ssp-github-index.html";
+  /* ref: https://stackoverflow.com/a/20717674/3931488 */
+  $url = "https://raw.githubusercontent.com/Vandivier/site-materials/master/public/index.html";
   $gethtml = file_get_contents($url);
   echo $gethtml; //output the content in the page...but should I be putting this in wp_footer?
 
---
+++ public/main.js
@@ -0,0 +1,1 @@
+console.log('testing this other file');
