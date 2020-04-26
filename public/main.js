b3383ddcab0b748b5a20e4fb18dfe6fbfc72bf17
Vandivier <john@afterecon.com>
2020-04-26 06:53:37 -0400

    github index.html as ref code only

--- public/main.js
+++ public/main.js
@@ -1,6 +1,5 @@
 // @ts-nocheck
 
 console.log("running site-materials main.js");
-
 document.querySelector("#masthead").remove();
 document.querySelector(".copyright-bar").innerHTML = `<span class="copyright">© 2020 After Economics</span>`;
