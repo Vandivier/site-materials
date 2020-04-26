18e61f6899d2dcf22574a0f69865b16a84ed560c
Vandivier <john@afterecon.com>
2020-04-26 07:31:34 -0400

    use jsdelivr
    add menu icon
    footer AfterEcon

--- public/main.js
+++ public/main.js
@@ -2,4 +2,4 @@
 
 console.log("running site-materials main.js");
 document.querySelector("#masthead").remove();
-document.querySelector(".copyright-bar").innerHTML = `<span class="copyright">© 2020 After Economics</span>`;
+document.querySelector(".copyright-bar").parentElement.innerHTML = `<span class="copyright">© 2020 AfterEcon</span>`;
